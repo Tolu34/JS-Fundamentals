6a90a04aa16a3cd1347abebc05b523e8a1ec8cac
Tech Girly <rebeccatolulope2@gmail.com>
2025-05-21 22:32:11 +0100

    Add script to print 'C is fun ' x times or show missing message

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,11 @@
 
+const x = parseInt(process.argv[2], 10);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
+
