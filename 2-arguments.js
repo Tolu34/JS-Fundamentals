69c8241277192e2e6b64270e5be2bde3b0683e15
Tech Girly <rebeccatolulope2@gmail.com>
2025-05-21 12:06:04 +0100

    Add arguments script with console logs

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
+=======
+const firstLine = 'No argument' ;
+const secondLine = 'Argument found' ;
+const thirdLine = 'Arguments found' ;
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
+
 
