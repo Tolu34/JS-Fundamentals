f823be72f27faba0ea9805c05cbf92e34f6e0751
Tech Girly <rebeccatolulope2@gmail.com>
2025-05-21 13:57:18 +0100

    Resolve merge conflicts in 5-to_integer.js and add script to print integer argument or 'Not a number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,13 @@
+const arg = process.argv[2];
+
+// parseInt converts the string to an integer or returns NaN if it can't
+const num = parseInt(arg, 10);
+
+// Check if num is a number using isNaN; if it's not a number, print "Not a number"
+if (!arg || isNaN(num)) {
+  console.log("Not a number");
+} else {
+  console.log("My number: " + num);
+}
+
 
